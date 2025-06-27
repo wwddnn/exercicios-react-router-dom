a23391fc449198232c5d2952af3b8a156fd01fa1
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-27 20:15:23 -0300

    Estrutura basica react router dom

---
+++ rotas-ex1/src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,9 @@
+
+export default function HomeBody() {
+
+    return(
+        <main>
+            <p>Página inicial</p>
+        </main>
+    );
+}
---
+++ rotas-ex1/src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Promotion() {
+
+    return (
+        <main>
+            <p>Página de promoção</p>
+        </main>
+    );
+}
