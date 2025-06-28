425f72a932fd9d6817f470e623759231f36eca4b
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-28 19:00:43 -0300

    ex2 finalizado

---
+++ rotas-ex2/src/components/Card/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+
+type Props = {
+    title : string;
+}
+
+export default function Card({ title }: Props) {
+
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+        </div>
+    );
+}
---
+++ rotas-ex2/src/routes/Subscription/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+
+export default function Subscription() {
+  return (
+    <>
+      <main>
+        <section>
+          <div className="container">
+            <h2 className="section-title sub-text">Faça sua inscrição!</h2>
+                <div className="mb30 mt30">
+                <Card title="Página de inscrição" />
+                </div>
+            <div className="dflex">
+                <Link to="/promotion">
+                    <Button text="Ver promoção" />
+                </Link>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
