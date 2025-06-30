3f139a00e7d9899cfab10bb8a48b9d5bd3df54bb
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-29 22:23:48 -0300

    ex3 finalizado

---
+++ rotas-ex3/src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+type Props = {
+  text: string;
+};
+
+export default function Button({ text }: Props) {
+  return (
+    <div className="button">
+      {text}
+    </div>
+  );
+}
